e85ce361dc4e1437162e726ca0f19e1ae9c81472
Phutanate Khoosirirat <57429412+newtiler@users.noreply.github.com>
2023-06-16 00:25:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,21 +25,6 @@ def get_recommendations(Name, cosine_sim=cosine_sim):
     anime_indices = [i[0] for i in sim_scores]
     return anime_df[['Name', 'Synopsis']].iloc[anime_indices]
 
-def Table(df):
-    fig=go.Figure(go.Table(columnorder = [1,2,3],
-          columnwidth = [100,100],
-            header=dict(values=['Name','Synopsis'],
-                        line_color='black',font=dict(color='white',size= 19),height=40,
-                        fill_color='red',
-                        align=['center','center']),
-                cells=dict(values=[df.Name,df.Synopsis],
-                       fill_color='#ffdac4',line_color='grey',
-                           font=dict(color='black', family="Lato", size=16),
-                       align='left')))
-    fig.update_layout(height=500, title ={'text': "Top 10 Anime Recommendations", 'font': {'size': 22}})
-    return st.plotly_chart(fig,use_container_width=True)
-anime_list = anime_df['Name'].values
-
 new_rating_df = rating_df.copy()
 
 cmat = pd.crosstab(new_rating_df['user_id'],new_rating_df['anime_id'],new_rating_df['rating'],aggfunc=sum)
@@ -64,36 +49,56 @@ def recommendation(uid,topn):
   res = res[['Name','rating']]
   return res
 
-def Table2(df2):
-    fig=go.Figure(go.Table(columnorder = [1,2,3],
-          columnwidth = [100,100],
-            header=dict(values=['Name','rating'],
-                        line_color='black',font=dict(color='white',size= 19),height=40,
-                        fill_color='red',
-                        align=['center','center']),
+def main():
+    st.title(':red[Anime] Recommendation System :movie_camera:')
+    anime_list = anime_df['Name'].values
+    types = ['--Select--', 'Content-Based', 'Collaborative Filtering']
+    type_sel = st.selectbox('Select Recommendation Type', types)
+    if type_sel == types[0]:
+        st.warning('Please select Recommendation Type!!')
+    elif type_sel == types[1]:
+        def Table(df):
+            fig=go.Figure(go.Table(columnorder = [1,2,3],
+                columnwidth = [100,100],
+                header=dict(values=['Name','Synopsis'],
+                    line_color='black',font=dict(color='white',size= 19),height=40,
+                    fill_color='red',
+                    align=['center','center']),
+                cells=dict(values=[df.Name,df.Synopsis],
+                    fill_color='#ffdac4',line_color='grey',
+                    font=dict(color='black', family="Lato", size=16),
+                    align='left')))
+            fig.update_layout(height=500, title ={'text': "Top 10 Anime Recommendations", 'font': {'size': 22}})
+            return st.plotly_chart(fig,use_container_width=True)
+        selected_anime = st.selectbox("Type or select an anime from the dropdown",anime_list)
+        if st.button('Show Recommendation'):
+            recommended_anime_names = get_recommendations(selected_anime)
+            #list_of_recommended_anime = recommended_anime_names.to_list()
+        # st.write(recommended_anime_names[['title', 'description']])
+            Table(recommended_anime_names)
+    elif type_sel == types[2]:
+        def Table2(df2):
+            fig=go.Figure(go.Table(columnorder = [1,2,3],
+                columnwidth = [100,100],
+                header=dict(values=['Name','rating'],
+                    line_color='black',font=dict(color='white',size= 19),height=40,
+                    fill_color='red',
+                    align=['center','center']),
                 cells=dict(values=[df2.Name,df2.rating],
-                       fill_color='#ffdac4',line_color='grey',
-                           font=dict(color='black', family="Lato", size=16),
-                       align='left')))
-    fig.update_layout(height=500, title ={'text': "Top 10 Anime Recommendations for user", 'font': {'size': 22}})
-    return st.plotly_chart(fig,use_container_width=True)
+                    fill_color='#ffdac4',line_color='grey',
+                    font=dict(color='black', family="Lato", size=16),
+                    align='left')))
+            fig.update_layout(height=500, title ={'text': "Top 10 Anime Recommendations for user", 'font': {'size': 22}})
+            return st.plotly_chart(fig,use_container_width=True)
+        selected_anime_fuser = round(st.number_input("Select user id that you want",min_value=recommend.index[0], max_value=recommend.index[-1]))
+        topn = round(st.number_input("Select number of recommendations",min_value=5, max_value=20, step=1))
+        if st.button('Show Recommendation for user'):
+            recommended_anime_fuser = recommendation(selected_anime_fuser, topn)
+        #list_of_recommended_anime = recommended_anime_fuser.to_list()
+        # st.write(recommended_anime_fuser[['title', 'description']])
+            Table2(recommended_anime_fuser)
+main()  
 
-st.title(':red[Anime] Recommendation System')
-selected_anime = st.selectbox("Type or select an anime from the dropdown",anime_list)
-if st.button('Show Recommendation'):
-    recommended_anime_names = get_recommendations(selected_anime)
-    #list_of_recommended_anime = recommended_anime_names.to_list()
-   # st.write(recommended_anime_names[['title', 'description']])
-    Table(recommended_anime_names)
-
-selected_anime_fuser = round(st.number_input("Select user id that you want",min_value=recommend.index[0], max_value=recommend.index[-1]))
-topn = round(st.number_input("Select number of recommendations",min_value=5, max_value=20, step=1))
-if st.button('Show Recommendation for user'):
-    recommended_anime_fuser = recommendation(selected_anime_fuser, topn)
-    #list_of_recommended_anime = recommended_anime_fuser.to_list()
-   # st.write(recommended_anime_fuser[['title', 'description']])
-    Table2(recommended_anime_fuser)
-    
 st.write('  '
          )
 st.write(' ')
