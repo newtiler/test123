4ac3c10b117d1aad0671c209fb7aa09f989dc124
Phutanate Khoosirirat <57429412+newtiler@users.noreply.github.com>
2023-06-15 14:32:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,24 +5,26 @@ import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-anime_df = pd.read_csv('anime_with_synopsis.csv')
-anime_df = anime_df.rename({'sypnopsis': 'Synopsis'}, axis=1)
-anime_df = anime_df.rename({'MAL_id': 'anime_id'}, axis=1)
+def main():
+    st.title('Anime Recommender System')
+    anime_df = pd.read_csv('anime_with_synopsis.csv')
+    anime_df = anime_df.rename({'sypnopsis': 'Synopsis'}, axis=1)
+    anime_df = anime_df.rename({'MAL_id': 'anime_id'}, axis=1)
 
-anime_copy = anime_df.copy()
-anime_copy = anime_copy[['Name', 'Genres', 'Synopsis']]
-anime_copy['tagline'] = anime_copy['Synopsis'] + anime_copy['Genres']
+    anime_copy = anime_df.copy()
+    anime_copy = anime_copy[['Name', 'Genres', 'Synopsis']]
+    anime_copy['tagline'] = anime_copy['Synopsis'] + anime_copy['Genres']
 
-tfidf = TfidfVectorizer(stop_words='english')
-anime_copy['tagline'] = anime_copy['tagline'].fillna('')
-tfidf_matrix = tfidf.fit_transform(anime_copy['tagline'])
+    tfidf = TfidfVectorizer(stop_words='english')
+    anime_copy['tagline'] = anime_copy['tagline'].fillna('')
+    tfidf_matrix = tfidf.fit_transform(anime_copy['tagline'])
 
-from sklearn.metrics.pairwise import linear_kernel
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    from sklearn.metrics.pairwise import linear_kernel
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-def get_recommendations(Name, cosine_sim=cosine_sim):
-    idx = anime_copy[anime_copy['Name'] == Name].index[0]
-    sim_scores = sorted(list(enumerate(cosine_sim[idx])), reverse=True, key=lambda x: x[1])[1:11]
-    anime_indices = [i[0] for i in sim_scores]
-    return anime_df[['Name', 'Synopsis']].iloc[anime_indices]
-st.write(anime_copy)
+    def get_recommendations(Name, cosine_sim=cosine_sim):
+        idx = anime_copy[anime_copy['Name'] == Name].index[0]
+        sim_scores = sorted(list(enumerate(cosine_sim[idx])), reverse=True, key=lambda x: x[1])[1:11]
+        anime_indices = [i[0] for i in sim_scores]
+        return anime_df[['Name', 'Synopsis']].iloc[anime_indices]
+main()
