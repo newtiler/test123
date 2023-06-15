91a6bc45745bd0870a1b13f3edb18f5190661f20
Phutanate Khoosirirat <57429412+newtiler@users.noreply.github.com>
2023-06-15 14:29:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ import plotly.graph_objects as go
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 anime_df = pd.read_csv('anime_with_synopsis.csv')
-st.write(anime_df)
+anime_df = anime_df.rename({'sypnopsis': 'Synopsis'}, axis=1)
+anime_df = anime_df.rename({'MAL_id': 'anime_id'}, axis=1)
+
+anime_copy = anime_df.copy()
+anime_copy = anime_copy[['Name', 'Genres', 'Synopsis']]
+anime_copy['tagline'] = anime_copy['Synopsis'] + anime_copy['Genres']
+st.write(anime_copy)
