64eff84205067f9b710f9bb5221c4e676ad2c291
Phutanate Khoosirirat <57429412+newtiler@users.noreply.github.com>
2023-06-15 14:36:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,27 @@ def main():
         sim_scores = sorted(list(enumerate(cosine_sim[idx])), reverse=True, key=lambda x: x[1])[1:11]
         anime_indices = [i[0] for i in sim_scores]
         return anime_df[['Name', 'Synopsis']].iloc[anime_indices]
+
+    def Table(df):
+    fig=go.Figure(go.Table(columnorder = [1,2,3],
+          columnwidth = [100,100],
+            header=dict(values=['Name','Synopsis'],
+                        line_color='black',font=dict(color='white',size= 19),height=40,
+                        fill_color='red',
+                        align=['center','center']),
+                cells=dict(values=[df.Name,df.Synopsis],
+                       fill_color='#ffdac4',line_color='grey',
+                           font=dict(color='black', family="Lato", size=16),
+                       align='left')))
+    fig.update_layout(height=500, title ={'text': "Top 10 Anime Recommendations", 'font': {'size': 22}})
+    return st.plotly_chart(fig,use_container_width=True)
+    anime_list = anime_df['Name'].values
+
+    selected_anime = st.selectbox("Type or select an anime from the dropdown",anime_list)
+    if st.button('Show Recommendation'):
+        recommended_anime_names = get_recommendations(selected_anime)
+    #list_of_recommended_anime = recommended_anime_names.to_list()
+   # st.write(recommended_anime_names[['title', 'description']])
+    Table(recommended_anime_names)
+    
 main()
